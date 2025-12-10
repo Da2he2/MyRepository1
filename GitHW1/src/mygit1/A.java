f3235d35d2b2557c104ab6ba4d2b7aa6d671ae4a
user <user@DESKTOP-PCCPM3D>
2025-12-10 11:41:54 +0900

    Bob revise A,B!

--- GitHW1/src/mygit1/A.java
+++ GitHW1/src/mygit1/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Alice !");
+		System.out.println("Bob revise A !");
 	}
 
 }
