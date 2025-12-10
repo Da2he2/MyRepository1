ab79734523704f06d33e74f224e474bacb54069d
user <user@DESKTOP-PCCPM3D>
2025-12-10 11:57:13 +0900

    master commit!

--- GitHW1/src/mygit1/A.java
+++ GitHW1/src/mygit1/A.java
@@ -4,16 +4,13 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 
 		System.out.println("Alice revise A ");
 		System.out.println("Hello Alice !");
 		System.out.println("Bob revise A !");
 		System.out.println("Bob revise A !");
 		System.out.println("Bob revise AA !");
-=======
 		System.out.println("Alice revise AA ");
->>>>>>> refs/heads/Alice
 	}
 
 }
